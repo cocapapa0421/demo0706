7d1569caa7f8842aec1b8809470776e27f61a8b3
cocapapa0421 <nvhuy.dev@gmail.com>
2021-06-13 09:09:29 +0700

    Fix components folder, add content into app.js file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,52 @@
+"use strict";
 import "./styles/styles.scss";
+
+// HTML Nodes
+const mobileMenu = document.querySelector(".jsMobMainMenu");
+const mobileNavigation = document.querySelector(".jsMobNavigation");
+const buttonOpenMobileMenu = document.querySelector(".jsOpenMobileMenu");
+const buttonOpenMobileSearch = document.querySelector(".jsOpenMobileSearch");
+
+// Helper function
+const setOverflowHidden = (val) =>
+  (document.documentElement.style.overflow = val);
+
+// Handler Function
+const handleSwitchMobileSubMenu = function (e) {
+  const target = e.target;
+
+  if (!target.classList.contains("mob-main-menu__link")) return;
+
+  const hasSubMenu = target.closest("li.has-sub-menu");
+
+  if (!hasSubMenu) return;
+  e.preventDefault();
+  this.querySelectorAll(".mob-main-menu__item").forEach((el) => {
+    if (el !== hasSubMenu) el.classList.remove("is-active");
+  });
+  hasSubMenu.classList.toggle("is-active");
+};
+
+const handleSwitchMobileNavigation = (e) => {
+  const button = e.target.closest(".mob-header__button");
+  const action = button.dataset.mobAction;
+  const openTarget = button.dataset.openTarget;
+
+  openTarget === "open-mobile-menu"
+    ? mobileNavigation.classList.remove("open-mobile-search")
+    : mobileNavigation.classList.remove("open-mobile-menu");
+  if (!action || !openTarget) return;
+
+  let icon = action === "open" ? "la-times" : "la-bars";
+
+  action === "open" ? setOverflowHidden("hidden") : setOverflowHidden("unset");
+  button.querySelector("i").classList.remove("la-bars", "la-times");
+  button.querySelector("i").classList.add(icon);
+  button.dataset.mobAction = action === "open" ? "close" : "open";
+  mobileNavigation.classList.toggle(openTarget);
+};
+
+// Add Event Listener
+mobileMenu.addEventListener("click", handleSwitchMobileSubMenu);
+buttonOpenMobileMenu.addEventListener("click", handleSwitchMobileNavigation);
+buttonOpenMobileSearch.addEventListener("click", handleSwitchMobileNavigation);
